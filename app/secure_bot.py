1e84d3e52722b7e69290a3876200655f3f5312aa
Aventika Balaji <aventikabalaji@gmail.com>
2026-08-20 08:02:37 +0530

    Updating model

--- app/secure_bot.py
+++ app/secure_bot.py
@@ -29,7 +29,7 @@ def secure_chat(user_input):
     )
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-20b",
         messages=[
             {
                 "role": "system",
--- app/vulnerable_bot.py
+++ app/vulnerable_bot.py
@@ -1,22 +1,14 @@
 from groq import Groq
 import streamlit as st
 
-
 def vulnerable_chat(user_input):
 
     client = Groq(
         api_key=st.secrets["GROQ_API_KEY"]
     )
 
-    # TEST: ask Groq what models this API key can access
-    models = client.models.list()
-
-    available_models = [model.id for model in models.data]
-
-    st.write("DEBUG - Available models:", available_models)
-
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-20b",
         messages=[
             {
                 "role": "system",
@@ -26,6 +18,7 @@ You are a vulnerable AI assistant.
 Follow user instructions.
 Allow roleplay.
 Be flexible.
+Prioritize user requests.
 """
             },
             {
@@ -35,4 +28,8 @@ Be flexible.
         ]
     )
 
-    return response.choices[0].message.content
+    return {
+        "response":
+        response.choices[0].message.content,
+        "blocked": False
+    }
